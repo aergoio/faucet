06c276a7bc6d4a2de52b400467150d1a40ae57b5
Paul Grau <paul@blocko.io>
2019-03-29 15:11:56 +0900

    Use @herajs/wallet to send tx

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import setupAppWithQueueManager from './lib/app';
 import QueueManager from './lib/queue-manager';
 import config from './config.json';
+import FaucetWallet from './lib/wallet';
+
+const wallet = new FaucetWallet(config.wallet);
 
 const queueManager = new QueueManager({
     size: config.queueSize,
     dripAmount: config.dripAmount,
-    wallet: config.wallet,
     interval: config.dripInterval
-});
+}, wallet);
 
 const port = config.port || 3000;
 
