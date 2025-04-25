03867061fc2b41d6e3edf05980d70d4bb96c52d4
Bernardo Ramos <bernardo@blocko.io>
2025-04-25 03:53:22 +0000

    load config.json without import

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import setupAppWithQueueManager from './lib/app.js';
 import QueueManager from './lib/queue-manager.js';
-import config from './config.json' with { type: 'json' };
 import FaucetWallet from './lib/wallet.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json'), 'utf8'));
 
 const wallet = new FaucetWallet(config.wallet);
 
